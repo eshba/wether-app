e1f8dfbf075894c85303b4e4ff00e0c347ff0680
eshba <banerjeeeshan9@gmail.com>
2019-09-02 23:04:27 +0530

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side Script Loaded!!')
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
